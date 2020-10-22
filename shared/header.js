2a1e0e7430b164eedbd4992cce8070bf291d4608
Sergio Carralero Nuño <sergio@MacBook-Pro-de-Sergio.local>
2020-10-22 15:03:56 +0200

    ended app

--- shared/header.js
+++ shared/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Header({ navigation, title }) {
@@ -9,17 +9,19 @@ export default function Header({ navigation, title }) {
     }
 
     return (
-        <View style={styles.header}>
+        <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
             <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
-            <View>
+            <View style={styles.headerTitle}>
+                <Image source={require('../assets/heart_logo.png')} style={styles.headerImage} />
                 <Text style={styles.headerText}>{ title }</Text>
             </View>
-        </View>
+        </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
     header: {
+        flex: 1,
         width: '100%',
         height: '100%',
         flexDirection: 'row',
@@ -35,5 +37,13 @@ const styles = StyleSheet.create({
     icon: {
         position: 'absolute',
         left: 0
+    },
+    headerImage: {
+        width: 26,
+        height: 26,
+        marginHorizontal: 10
+    },
+    headerTitle: {
+        flexDirection: 'row'
     }
 });
